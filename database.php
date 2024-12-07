d0990e25ce0cf80fa11f1a52e952ebafe9004b92
mrgaren3 <121166488+mrgaren3@users.noreply.github.com>
2024-12-07 22:18:51 +0200

    Add files via upload

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+// Database configuration
+$servername = "localhost"; // Change if your database server is on a different host
+$username = "root";        // Replace with your database username
+$password = "";            // Replace with your database password
+$dbname = "NovelWebsite";  // The name of your database
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to the database";
+?>
